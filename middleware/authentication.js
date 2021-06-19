b90af10b146d24b8978cd144c3614c02d6f4b4b8
dafasr <85047424+dafasr@users.noreply.github.com>
2021-06-19 12:33:35 +0700

    Add files via upload

---
+++ middleware/authentication.js
@@ -0,0 +1,21 @@
+const sql = require('mysql')
+const { rows } = require("mssql");
+const con = sql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: '',
+    database: 'tugas1'
+})
+
+module.exports = function(req, res, next) {
+    const username = req.headers.username
+    const password = req.headers.password
+
+    con.query('SELECT username FROM user WHERE username=? AND password=?', [username, password], function(err, data1){
+        if(data1.length > 0 ) {
+            next()
+        } else {
+            res.send(401)
+        }
+    })
+}
